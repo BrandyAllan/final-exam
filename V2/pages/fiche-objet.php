4215909889c77e11f944e0b4fce54f18d381b250
BrandyAllan <brandyratsimbaharison46@gmail.com>
2025-07-14 14:01:30 +0300

    add carousel

--- V2/pages/fiche-objet.php
+++ V2/pages/fiche-objet.php
@@ -11,6 +11,31 @@ $history_emprunt = get_objet_emprunt_history($_GET['id_objet']);
 </head>
 <body>
     <section>
+        <div id="mainCarousel" class="carousel slide mb-3" data-bs-ride="carousel">
+            <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="../assets/images/<?= $donnee['type_maison'] ?>.jpeg" class="d-block w-100" alt="Room 1">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/cuisine<?= $donnee['type_maison'] ?>.jpeg" class="d-block w-100" alt="Room 2">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/chambre<?= $donnee['type_maison'] ?>.jpeg" class="d-block w-100" alt="Room 3">
+            </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#mainCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#mainCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon"></span>
+            </button>
+        </div>
+
+        <div class="d-flex gallery-thumbs">
+            <img src="../assets/images/<?= $donnee['type_maison'] ?>.jpeg" data-bs-target="#mainCarousel" data-bs-slide-to="0" class="me-2">
+            <img src="../assets/images/cuisine<?= $donnee['type_maison'] ?>.jpeg" data-bs-target="#mainCarousel" data-bs-slide-to="1">
+            <img src="../assets/images/chambre<?= $donnee['type_maison'] ?>.jpeg" data-bs-target="#mainCarousel" data-bs-slide-to="2">
+        </div>
         <article>
             <h3>Histprique d'emprunt</h3>
             <table class="table">
